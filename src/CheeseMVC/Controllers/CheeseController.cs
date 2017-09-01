2361142f9ee61d0cef29666b348ad1c676c73b7a
softprodev <softprodev15@gmail.com>
2017-09-01 13:25:30 -0500

    Migrate to VS 2017

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,14 +13,14 @@ namespace CheeseMVC.Controllers
     public class CheeseController : Controller
     {
 
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
-
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.cheeses = Cheeses;
+            CheeseListViewModel model = new CheeseListViewModel {
+                Cheeses = CheeseData.GetAll()
+            };
 
-            return View();
+            return View(model);
         }
 
         public IActionResult Add()
@@ -28,12 +30,20 @@ namespace CheeseMVC.Controllers
 
         [HttpPost]
         [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name, string description = "")
+        public IActionResult NewCheese(string name, string description)
         {
             // Add the new cheese to my existing cheeses
-            Cheeses.Add(name, description);
+            CheeseData.Add(new Cheese(name, description));
 
-            return Redirect("/Cheese");
+            return Redirect("/");
         }
+
+        public IActionResult Detail(int id)
+        {
+            ViewBag.cheese = CheeseData.GetById(id);
+            ViewBag.title = "Cheese Detail";
+            return View();
+        }
+
     }
 }
